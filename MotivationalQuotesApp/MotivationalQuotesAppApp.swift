8606b0f4b6b8a975ee58519dfba50844907b86e7
Tony-Kara <eniolaanthony@gmail.com>
2021-09-25 22:08:51 +0600

    Initial Commit

---
+++ MotivationalQuotesApp/MotivationalQuotesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotivationalQuotesAppApp.swift
+//  MotivationalQuotesApp
+//
+//  Created by mac on 9/25/21.
+//
+
+import SwiftUI
+
+@main
+struct MotivationalQuotesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
